b731522456261bb20b6186ddf4cfd9b4fa967a1b
Aleksandra Wilczynska <a.e.wilczynska@tudelft.nl>
2024-12-04 08:40:06 +0100

    day 3, part 2

---
+++ R/day03.R
@@ -0,0 +1,42 @@
+# Load libraries
+library(here)
+library(stringr)
+
+
+
+# Load the data -----------------------------------------------------------
+text_d3 <-readLines(here("data", "day03_input.txt"))
+
+# Part 1 ----------------------------------------------------------------
+
+muls <- str_extract_all(text_d3, "mul\\([0-9]{1,3},[0-9]{1,3}\\)") |>
+  unlist() |>
+  str_replace_all(",", "*") |>
+  str_remove_all("^(mul)") |>
+  sapply(function(x) eval(parse(text=x))) |>
+  sum()
+
+# Part 2 ----------------------------------------------------------------
+
+# include do's and don'ts
+muls_do <- str_extract_all(text_d3,
+                           "(mul\\([0-9]{1,3},[0-9]{1,3}\\))|(don't\\(\\))|(do\\(\\))") |>
+  unlist() |>
+  str_replace_all(",", "*") |>
+  str_remove_all("^(mul)")
+
+
+# loop with a multiplier (mul_status) to 'swtich on' or 'switch off'calculation
+counter <- 0
+mul_status <- 1
+for (i in 1: length(muls_do)) {
+  if (!muls_do[i] %in%  c("don't()", "do()" ))
+    counter <- counter + (eval(parse(text=muls_do[i])))* mul_status
+  if (muls_do[i]==  "don't()")
+    mul_status <- 0
+  if (muls_do[i] ==  "do()")
+    mul_status <- 1
+
+}
+
+
